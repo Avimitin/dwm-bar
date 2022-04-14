f79d3b5079caf361a183acdb3b3511e1da7a9ef3
Avimitin <avimitin@gmail.com>
2022-04-14 17:41:11 +0800

    reft! rewrite mpris crate with dbus
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- src/component/song.rs
+++ src/component/song.rs
@@ -1,15 +1,18 @@
-use mpris::PlayerFinder;
 use super::widget::Block;
+use anyhow::Result;
+use dbus::arg;
+use dbus::blocking::{Connection, stdintf::org_freedesktop_dbus::Properties};
+use std::time::Duration;
 
 pub fn song_info() -> Option<Block> {
-    // TODO: We need to use logging to report error here.
-    let player = PlayerFinder::new().ok()?.find_active().ok()?;
+    let conn = Connection::new_session().ok()?;
+    let player_addr = find_active_player_address(&conn).ok()?;
+    let metadata = get_metadata(&conn, &player_addr).ok()?;
 
-    let text_limit = 40;
-    let metadata = player.get_metadata().ok()?;
-
-    let artist = metadata.artists()?.join(" ");
-    let song = metadata.title()?;
+    let artist: Option<&Vec<String>> = arg::prop_cast(&metadata, "xesam:artist");
+    let artist = artist?.join(" ");
+    let song: Option<&String> = arg::prop_cast(&metadata, "xesam:title");
+    let song = song?;
 
     let output = format!(
         " {} - {} ",
@@ -21,6 +24,7 @@ pub fn song_info() -> Option<Block> {
         song,
     );
 
+    let text_limit = 40;
     // trim the text
     let output = if output.len() > text_limit {
         let split = output.chars().take(text_limit).collect::<String>();
@@ -34,4 +38,22 @@ pub fn song_info() -> Option<Block> {
             .icon_color("#EAEAEA", "#0C0C0C")
             .text_color("#EAEAEA", "#171617"),
     )
+}
+
+fn find_active_player_address(conn: &Connection) -> Result<String> {
+    let proxy = conn.with_proxy("org.freedesktop.DBus", "/", Duration::from_millis(2000));
+    let (services,): (Vec<String>,) = proxy.method_call("org.freedesktop.DBus", "ListNames", ())?;
+
+    for service in services {
+        if service.contains("mpris") {
+            return Ok(service);
+        }
+    }
+
+    anyhow::bail!("No active mpris player was found")
+}
+
+fn get_metadata(conn: &Connection, addr: &str) -> Result<arg::PropMap> {
+    let proxy = conn.with_proxy(addr, "/org/mpris/MediaPlayer2", Duration::from_millis(2000));
+    Ok(proxy.get("org.mpris.MediaPlayer2.Player", "Metadata")?)
 }
