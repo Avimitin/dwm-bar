65d7016b1136272d36ad9929563f2d0c56879afb
Avimitin <avimitin@gmail.com>
2022-04-14 15:22:56 +0800

    reft! simplify the battery percentage fetcher
    The laptop battery functionality should be basic and can be ran without
    dependency. So here I use the system information instead of dbus.
    
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- src/component/battery.rs
+++ src/component/battery.rs
@@ -2,24 +2,41 @@ use super::widget::Block;
 use dbus::blocking::stdintf::org_freedesktop_dbus::Properties;
 use dbus::blocking::Connection;
 use dbus::Path;
-use std::sync::{Arc, Mutex};
+use std::fs;
 use std::time::Duration;
 
-lazy_static::lazy_static!(
-    /// Global DBus connection, can be shared between thread.
-    static ref DBUS_SYSTEM: Arc<Mutex<Connection>> = Arc::new(
-        Mutex::new(
-            Connection::new_system()
-            .expect(
-                "Fail to connect to dbus, please ensure you are running in Linux with DBus, or ensure you have DBus daemon enabled."
-            )
-        ));
-);
+/// Build a component to show laptop battery percentage and power-supply status.
+/// Statistic come from /sys/class/power_supply/<bat_name>/{capacity,status}.
+///
+/// Return None if no battery device name contains "BAT0" keyword,
+/// or no capacity/status file was found.
+pub fn battery() -> Option<Block> {
+    let perc: u32 = fs::read_to_string(format!("/sys/class/power_supply/{}/capacity", "BAT0"))
+        .ok()?
+        .parse()
+        .ok()?;
+    let stat = fs::read_to_string(format!("/sys/class/power_supply/{}/status", "BAT0")).ok()?;
 
-/// Enumerate through the devices, return path with pattern matched.
-fn get_device_path(pat: &str) -> Option<Path> {
-    let conn = DBUS_SYSTEM.lock().unwrap();
-    let proxy = conn.with_proxy(
+    let icon = if stat == "Discharging" { "" } else { "" };
+
+    Some(
+        Block::new(icon, format!("{} %", perc))
+            .text_fg("#EAEAEA")
+            .icon_fg("#EAEAEA"),
+    )
+}
+
+/// Build a headset battery component.
+/// This functionality depends on UPower DBus daemon
+///
+/// Return None if no battery device name contains "headset" keyword,
+/// or no percentage property is found.
+pub fn headset_battery() -> Option<Block> {
+    let dbus_sys_connection = Connection::new_system()
+        .expect(
+            "Fail to connect to dbus, please ensure you are running in Linux with DBus, or ensure you have DBus daemon enabled."
+        );
+    let proxy = dbus_sys_connection.with_proxy(
         "org.freedesktop.UPower",
         "/org/freedesktop/UPower",
         Duration::from_millis(2000),
@@ -31,56 +48,17 @@ fn get_device_path(pat: &str) -> Option<Path> {
 
     let mut device = Path::default();
     for dev in devices {
-        if dev.contains(pat) {
+        if dev.contains("headset") {
             device = dev;
             break;
         }
     }
 
     if device.is_empty() {
-        None
-    } else {
-        Some(device)
+        return None;
     }
-}
 
-/// Build a component to show laptop battery percentage and power-supply status.
-///
-/// Return None if no battery device name contains "BAT0" keyword,
-/// or PowerSupply/Percentage property are not found.
-pub fn battery() -> Option<Block> {
-    let device = get_device_path("BAT0")?;
-    let conn = DBUS_SYSTEM.lock().unwrap();
-    let proxy = conn.with_proxy(
-        "org.freedesktop.UPower",
-        device,
-        Duration::from_millis(2000),
-    );
-
-    let has_power_supply: bool = proxy
-        .get("org.freedesktop.UPower.Device", "PowerSupply")
-        .ok()?;
-    let percentage: f64 = proxy
-        .get("org.freedesktop.UPower.Device", "Percentage")
-        .ok()?;
-
-    let icon = if has_power_supply { "" } else { "" };
-
-    Some(
-        Block::new(icon, format!("{:.0} %", percentage))
-            .text_fg("#EAEAEA")
-            .icon_fg("#EAEAEA"),
-    )
-}
-
-/// Build a headset battery component.
-///
-/// Return None if no battery device name contains "headset" keyword,
-/// or no percentage property is found.
-pub fn headset_battery() -> Option<Block> {
-    let device = get_device_path("headset")?;
-    let conn = DBUS_SYSTEM.lock().unwrap();
-    let proxy = conn.with_proxy(
+    let proxy = dbus_sys_connection.with_proxy(
         "org.freedesktop.UPower",
         device,
         Duration::from_millis(2000),
