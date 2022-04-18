a17b1f5ad296f35a6f1bfd2c6b2f6303dea7315e
Avimitin <avimitin@gmail.com>
2022-04-18 15:09:41 +0800

    battery: read file concurrently
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- src/component/battery.rs
+++ src/component/battery.rs
@@ -7,17 +7,23 @@ use tokio::fs;
 /// Return None if no battery device name contains "BAT0" keyword,
 /// or no capacity/status file was found.
 pub async fn battery() -> Option<Block> {
-    let perc: u32 = fs::read_to_string(format!("/sys/class/power_supply/{}/capacity", "BAT0"))
-        .await
-        .ok()?
-        .parse()
-        .ok()?;
+    let perc = tokio::spawn(async {
+        Some(
+            fs::read_to_string(format!("/sys/class/power_supply/{}/capacity", "BAT0"))
+                .await
+                .ok()?
+                .parse::<i32>()
+                .ok()?,
+        )
+    });
 
     let stat = fs::read_to_string(format!("/sys/class/power_supply/{}/status", "BAT0"))
         .await
         .ok()?;
 
     let icon = if stat == "Discharging" { "" } else { "" };
+
+    let perc = perc.await.unwrap()?;
 
     Some(
         Block::new(icon, format!("{} %", perc))
@@ -27,13 +33,13 @@ pub async fn battery() -> Option<Block> {
 }
 
 #[cfg(feature = "bluetooth-battery")]
+use dbus::nonblock::{stdintf::org_freedesktop_dbus::Properties, Proxy};
+#[cfg(feature = "bluetooth-battery")]
 use dbus::Path;
 #[cfg(feature = "bluetooth-battery")]
 use dbus_tokio::connection;
 #[cfg(feature = "bluetooth-battery")]
 use std::time::Duration;
-#[cfg(feature = "bluetooth-battery")]
-use dbus::nonblock::{stdintf::org_freedesktop_dbus::Properties, Proxy};
 
 /// Build a headset battery component.
 /// This functionality depends on UPower DBus daemon
