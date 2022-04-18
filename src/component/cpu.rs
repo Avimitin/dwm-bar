11243dcf4984b55d8c784c729876633aaad88229
Avimitin <avimitin@gmail.com>
2022-04-18 12:53:49 +0800

    cpu: split load_avg function logic
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- src/component/cpu.rs
+++ src/component/cpu.rs
@@ -3,13 +3,18 @@ use std::time::Duration;
 use tokio::fs;
 use tokio::time::sleep;
 
-pub async fn avg_load() -> Option<Block> {
+struct CpuStat {
+    sum: i32,
+    active: i32,
+}
+
+async fn get_stats() -> Option<CpuStat> {
     let status = fs::read_to_string("/proc/stat").await.ok()?;
-    let mut before = Vec::new();
+    let mut time = Vec::new();
     for line in status.lines() {
         if line.starts_with("cpu") {
             // remove the "cpu" prefix
-            before = line
+            time = line
                 .split_whitespace()
                 .skip(1)
                 .map(|x| {
@@ -21,48 +26,31 @@ pub async fn avg_load() -> Option<Block> {
         }
     }
 
-    if before.len() < 8 {
+    if time.len() < 8 {
         return None;
     }
+
+    let sum: i32 = time.iter().sum();
+    // idle + iowait
+    let inactive = time[3] + time[4];
+
+    Some(CpuStat {
+        sum,
+        active: sum - inactive,
+    })
+}
+
+pub async fn avg_load() -> Option<Block> {
+    let before = get_stats().await?;
 
     sleep(Duration::from_secs(1)).await;
 
-    let status = fs::read_to_string("/proc/stat").await.ok()?;
-    let mut after = Vec::new();
-    for line in status.lines() {
-        if line.starts_with("cpu") {
-            // remove the "cpu" prefix
-            after = line
-                .split_whitespace()
-                .skip(1)
-                .map(|x| {
-                    x.parse::<i32>()
-                        .expect("Fail to parse the /proc/stat file, please check your system")
-                })
-                .collect();
-            break;
-        }
-    }
+    let after = get_stats().await?;
 
-    if after.len() < 8 {
-        return None;
-    }
-
-    // get the cpu idle time
-    let before_sum: i32 = before.iter().sum();
-    let after_sum: i32 = after.iter().sum();
-
-    let sum = (before_sum - after_sum) as f32;
-
-    let before_idle: i32 = before[3];
-    let before_iowait: i32 = before[4];
-    let before_sum = (before_sum - before_idle - before_iowait) as f32;
-
-    let after_idle: i32 = after[3];
-    let after_iowait: i32 = after[4];
-    let after_sum = (after_sum - after_idle - after_iowait) as f32;
-
-    let avg = (before_sum - after_sum) / sum;
+    // get total
+    let sum = (before.sum - after.sum) as f32;
+    // use active time / total
+    let avg = ((before.active - after.active) as f32) / sum;
 
     Some(
         Block::new("﬙", format!("{:.2} %", avg * 100.0))
