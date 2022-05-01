7efe0f0064e97246dda82a9fecf7ce31737d28e2
Avimitin <avimitin@gmail.com>
2022-05-01 19:06:58 +0800

    date: impl Updater trait for datetime
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- src/component/datetime.rs
+++ src/component/datetime.rs
@@ -2,12 +2,34 @@
 use super::widget::Block;
 use chrono::prelude::Local;
 
-pub fn date_and_time() -> Option<Block> {
-    let now = Local::now();
-    Some(
-        Block::new("", now.format("%B/%d %I:%M %p").to_string())
-            .text_fg("#EAEAEA")
-            .icon_fg("#EAEAEA"),
-    )
+pub struct DateTime {
+    format: String,
+    icon: String,
 }
 
+impl Default for DateTime {
+    fn default() -> Self {
+        Self {
+            format: "%B/%d %I:%M %p".to_string(),
+            icon: "".to_string(),
+        }
+    }
+}
+
+impl DateTime {
+    fn new<T: Into<String>, K: Into<String>>(icon: T, format: K) -> Self {
+        Self { icon: icon.into(), format: format.into() }
+    }
+}
+
+#[async_trait::async_trait]
+impl super::Updater for DateTime {
+    async fn update(&mut self) -> Option<Block> {
+        let now = Local::now();
+        Some(
+            Block::new(&self.icon, now.format(&self.format).to_string())
+                .text_fg("#EAEAEA")
+                .icon_fg("#EAEAEA"),
+        )
+    }
+}
